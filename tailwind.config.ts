8055aaa3c1caf14a4f75d11771f672c1f3f57f25
Mohammad Mushfiq Us Saleheen <aurpon80@gmail.com>
2026-01-06 10:38:11 +0600

    feat: remove deprecated collections and products JSON files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
-import type { Config } from "tailwindcss";
+// import type { Config } from "tailwindcss";
 
-const config: Config = {
+const config = {
   darkMode: "class",
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
