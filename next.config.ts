93c3a19bea2b04d9b45124e2d53fe41c84b2750b
AkrAm <ak.khan.kp@gmail.com>
2026-01-09 16:21:28 +0600

    `cacheComponents`

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    cacheComponents: true, // 
     unoptimized: true,
     minimumCacheTTL: 604800, // Cache images for one week (in seconds)
 
